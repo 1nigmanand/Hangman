4ce8866c6077934bc3548525d1ab2bd6a4fa327c
Black_star22 <blackstar7608@gmail.com>
2022-10-01 13:47:17 +0530

    hangman

---
+++ hangman.js
@@ -0,0 +1,90 @@
+// var hangman = [`+--------+
+//           |
+//           |
+//           |
+//           |
+//           |
+// ___________`,
+// `+--------+
+//     |     |
+//           |
+//           |
+//           |
+//           |
+// ___________`,
+// `+--------+
+//     |     |
+//     0     |
+//           |
+//           |
+//           |
+//           |
+// ___________`,
+// `+--------+
+//     |     |
+//     0     |
+//     |     |
+//           |
+//           |
+// ___________`,
+// `+--------+
+//     |     |
+//     0     |
+//    /|\\    |
+//           |
+//           |
+// ___________`,
+// `+--------+
+//     |     |
+//     0     |
+//    /|\\    |
+//    / \\    |
+//           |
+// ___________`];
+
+
+// const input = require("readline-sync")
+// let list = ["nigmanand","santosh","ajay","madhu","sheikh"]
+
+// while (true){
+//     var num = Math.floor(Math.random()*10)
+//     if (num < list.length)break
+// }
+// var int = 0
+// let mainstring = list[num];
+// console.log(mainstring);
+// while (true){
+//     let single_alphabet = input.question("Enter alphabet-:")
+//     if (single_alphabet.length=1){
+//         if (mainstring.search(single_alphabet)!==-1){
+//             console.log("guess is correct.");
+//             let substring = mainstring.replace(single_alphabet,"")
+//             mainstring = substring
+
+//         }
+//         else if (int == hangman.length-1){
+//             console.log(hangman[int]);
+//             console.log("you are dead:");
+//             break
+//         }
+//         else{
+//             console.log(hangman[int]);
+//             int++
+//         }
+//         if(Boolean(mainstring)==false){
+//             console.log("you won.\nCongrats");
+//             break
+//         }
+
+        
+
+
+
+
+
+//     }
+//     else{
+//         print("Input is out of range:")cd
+//     }
+// }
+
